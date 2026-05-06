5cca2ff145aa9bfeebf90ce5dd2806e9222349a9
vishnuvaradhans <vishnuvaradhansoff757@gmail.com>
2026-05-06 13:18:41 +0530

    Created UC6

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,30 +1,21 @@
-public class TicTacToe{
+/**
+ * TicTacToe
+ * UC6 places a player's symbol on the board at the given position.
+ * This use case focuses on updating game state.
+ */
+
+public class TicTacToe {
 
     static char[][] board = new char[3][3];
 
     public static void main(String[] args) {
-        initializeBoard();
-        printBoard();
-    };
 
-    
-
-    static void printBoard(){
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-        
+        placeMove(0, 0, 'X');
+        System.out.println(board[0][0]);
     }
 
-    static void initializeBoard(){
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
-                board[i][j] = '-';
-            }
-        }
-    }
+    static void placeMove(int row, int col, char symbol) {
 
+        board[row][col] = symbol;
+    }
 }
